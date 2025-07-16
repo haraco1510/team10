b8fdce4d9f02462281415073e077e1a91b03e54b
okuma624 <m251982@hiroshima-u.ac.jp>
2025-07-16 10:12:13 +0900

    marge 2

--- main.go
+++ main.go
@@ -14,12 +14,42 @@ func main(){
 	printBoard(board, revealed)
 
 	var num_x, num_y int
-	fmt.Scanf("%d %d", &num_x, &num_y)
-	var bom_x=2
-	var bom_y=3
+	var result int
 	
-	result := input(num_x, num_y)
-	fmt.Println(result)
-	explosion := Compare(bom_x, bom_y, num_x, num_y)
-	fmt.Println(explosion)
+	fmt.Print("Start!")
+
+	for{
+		fmt.Print("数字を入力してね！：")
+		fmt.Scanf("%d %d", &num_x, &num_y)
+		inputRange := input(num_x, num_y)
+		if(inputRange == false){
+			continue
+		}
+		revealed[num_x][num_y] = true
+		explosion := Compare(board, num_x, num_y)
+		if(explosion == true){
+			result = -1
+			break
+		}
+		mul=1
+		for i:=0 ;i<Rows;i++{
+			for j:=0;j<Cols;j++{
+				mul = mul*revealed[i][j]
+			}
+		}
+		if(mul){
+			result = 1
+			break
+		}
+		//fmt.Println(explosion)
+		printBoard(board, revealed)
+	}
+
+	if(result == -1){
+		fmt.Print("fail")
+	}else{
+		fmt.Print("success")
+	}
+	
+	
 }
